8fabccdcdd541d30d64d197ed41f5b68706d9bd1
N Ford <nford@....com>
2014-12-31 18:01:49 +0000

    Add python package bjoern to buildroot

---
+++ package/python-bjoern/python-bjoern.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-bjoern
+#
+################################################################################
+
+PYTHON_BJOERN_VERSION = 1.4.1
+PYTHON_BJOERN_SOURCE = bjoern-$(PYTHON_BJOERN_VERSION).tar.gz
+PYTHON_BJOERN_SITE = https://pypi.python.org/packages/source/b/bjoern/
+PYTHON_BJOERN_LICENSE = BSD
+PYTHON_BJOERN_LICENS_FILES = LICENSE
+PYTHON_BJOERN_SETUP_TYPE = distutils
+
+$(eval $(python-package))
