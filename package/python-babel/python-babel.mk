1d97a972734e00a9d351d938c07c851d39db77ee
N Ford <nford@....com>
2015-03-11 17:42:52 +0000

    Add Babel python module to buildroot

---
+++ package/python-babel/python-babel.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-bjoern
+#
+################################################################################
+
+PYTHON_BABEL_VERSION = 1.3
+PYTHON_BABEL_SOURCE = Babel-$(PYTHON_BABEL_VERSION).tar.gz
+PYTHON_BABEL_SITE = https://pypi.python.org/packages/source/B/Babel/
+PYTHON_BABEL_LICENSE = BSD
+PYTHON_BABEL_LICENS_FILES = LICENSE
+PYTHON_BABEL_SETUP_TYPE = setuptools
+
+$(eval $(python-package))
