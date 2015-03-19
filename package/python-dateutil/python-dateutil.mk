430af88808a2141caab223253903386751c40198
N Ford <nford@....com>
2015-03-19 18:21:40 +0000

    python-six is a dependency of dateutil

--- package/python-dateutil/python-dateutil.mk
+++ package/python-dateutil/python-dateutil.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-PYTHON_DATEUTIL_VERSION = 1.5
+PYTHON_DATEUTIL_VERSION = 2.4.1
 PYTHON_DATEUTIL_SOURCE = python-dateutil-$(PYTHON_DATEUTIL_VERSION).tar.gz
 PYTHON_DATEUTIL_SITE = https://pypi.python.org/packages/source/p/python-dateutil/
 PYTHON_DATEUTIL_LICENSE = BSD
+PYTHON_DATEUTIL_LICENSE_FILES = LICENSE
 PYTHON_DATEUTIL_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
