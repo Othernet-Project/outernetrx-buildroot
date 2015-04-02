e279c4820de08e03758f23c5d41c0417b5f4a917
N Ford <nford@....com>
2015-04-02 16:23:17 +0000

    Bump pytz version to 2015.2

--- package/python-pytz/python-pytz.mk
+++ package/python-pytz/python-pytz.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PYTZ_VERSION = 2014.10
+PYTHON_PYTZ_VERSION = 2015.2
 PYTHON_PYTZ_SOURCE = pytz-$(PYTHON_PYTZ_VERSION).tar.bz2
 PYTHON_PYTZ_SITE = https://pypi.python.org/packages/source/p/pytz/
 PYTHON_PYTZ_LICENSE = MIT
