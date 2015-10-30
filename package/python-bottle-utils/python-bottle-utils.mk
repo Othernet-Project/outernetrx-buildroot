9eac3875c46929627f55f37b88c8feefc077a586
Branko Vukelic <branko@outernet.is>
2015-10-30 22:12:09 +0100

    Updated bottle-utils
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- package/python-bottle-utils/python-bottle-utils.mk
+++ package/python-bottle-utils/python-bottle-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_BOTTLE_UTILS_VERSION = 0.6.post1
+PYTHON_BOTTLE_UTILS_VERSION = 0.7
 PYTHON_BOTTLE_UTILS_SOURCE = bottle-utils-$(PYTHON_BOTTLE_UTILS_VERSION).tar.gz
 PYTHON_BOTTLE_UTILS_SITE = http://pypi.python.org/packages/source/b/bottle-utils/
 PYTHON_BOTTLE_UTILS_LICENSE = GPL
