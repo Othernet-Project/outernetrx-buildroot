d7c5b656a737e5e3f6913dee2c3fdcadee08ab79
Branko Vukelic <branko@outernet.is>
2015-11-04 14:21:21 +0100

    Added librarian-diskspace package
    Signed-off-by: Branko Vukelic <branko@outernet.is>

---
+++ package/python-librarian-diskspace/python-librarian-diskspace.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-librarian-diskspace
+#
+################################################################################
+
+PYTHON_LIBRARIAN_DISKSPACE_VERSION = 0c370024ea9c23b953878226cf2f2f1d392e00cb
+PYTHON_LIBRARIAN_DISKSPACE_SOURCE = $(PYTHON_LIBRARIAN_DISKSPACE_VERSION).tar.gz
+PYTHON_LIBRARIAN_DISKSPACE_SITE = https://github.com/Outernet-Project/librarian-diskspace/archive/
+PYTHON_LIBRARIAN_DISKSPACE_LICENSE = GPL
+PYTHON_LIBRARIAN_DISKSPACE_DEPENDENCIES = python-librarian-core python-librarian-dashboard python-hwd
+PYTHON_LIBRARIAN_DISKSPACE_SETUP_TYPE = setuptools
+
+$(eval $(python-package))
