d2f2ed515f65790961cc9bca6c92243c0b5a1021
Branko Vukelic <branko@outernet.is>
2015-11-06 17:00:03 +0100

    Updated librarian-ui
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- package/python-librarian-ui/python-librarian-ui.mk
+++ package/python-librarian-ui/python-librarian-ui.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_UI_VERSION = d185f9cfe91a434f583e5365fdb29cfb9ecf98c7
+PYTHON_LIBRARIAN_UI_VERSION = a2fb5253ed6696b0b14a36b7e3a5574d2d79ebb6
 PYTHON_LIBRARIAN_UI_SOURCE = $(PYTHON_LIBRARIAN_UI_VERSION).tar.gz
 PYTHON_LIBRARIAN_UI_SITE = https://github.com/Outernet-Project/librarian-ui/archive/
 PYTHON_LIBRARIAN_UI_LICENSE = GPL
