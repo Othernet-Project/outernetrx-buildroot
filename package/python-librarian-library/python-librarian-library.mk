104e33aede7d99173b31413dcfae6fc0c2782e50
Branko Vukelic <branko@outernet.is>
2015-11-09 17:57:59 +0100

    Updated packages
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- package/python-librarian-library/python-librarian-library.mk
+++ package/python-librarian-library/python-librarian-library.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_LIBRARY_VERSION = 982564dea9e8cf03186ab7d5e0a5c553752c1ff3
+PYTHON_LIBRARIAN_LIBRARY_VERSION = 167f69fda972651e18e4e72e0f2aeeff50c93c24
 PYTHON_LIBRARIAN_LIBRARY_SOURCE = $(PYTHON_LIBRARIAN_LIBRARY_VERSION).tar.gz
 PYTHON_LIBRARIAN_LIBRARY_SITE = https://github.com/Outernet-Project/librarian-library/archive/
 PYTHON_LIBRARIAN_LIBRARY_LICENSE = GPL
