da939f8cc609492c5277f5371c2a6c17e753a578
Branko Vukelic <branko@outernet.is>
2015-12-08 18:39:05 +0100

    Set correct version for ondd-ipc
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- package/python-ondd-ipc/python-ondd-ipc.mk
+++ package/python-ondd-ipc/python-ondd-ipc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_ONDD_IPC_VERSION = 0.8.post1
+PYTHON_ONDD_IPC_VERSION = 1e3f648935b2c99267c16d83ba052bfaa5f0b8b1
 PYTHON_ONDD_IPC_SOURCE = $(PYTHON_ONDD_IPC_VERSION).tar.gz
 PYTHON_ONDD_IPC_SITE = https://github.com/Outernet-Project/ondd-ipc/archive
 PYTHON_ONDD_IPC_LICENSE = GPL
