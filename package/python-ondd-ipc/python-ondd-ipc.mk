4a4fc69f394f4ba6bffb9507b4d98f9772ca47b7
Branko Vukelic <branko@outernet.is>
2015-12-08 18:31:06 +0100

    Added ondd-ipc package
    Signed-off-by: Branko Vukelic <branko@outernet.is>

---
+++ package/python-ondd-ipc/python-ondd-ipc.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# python-ondd-ipc
+#
+################################################################################
+
+PYTHON_ONDD_IPC_VERSION = 0.8.post1
+PYTHON_ONDD_IPC_SOURCE = $(PYTHON_ONDD_IPC_VERSION).tar.gz
+PYTHON_ONDD_IPC_SITE = https://github.com/Outernet-Project/ondd-ipc/archive
+PYTHON_ONDD_IPC_LICENSE = GPL
+PYTHON_ONDD_IPC_SETUP_TYPE = setuptools
+
+$(eval $(python-package))
