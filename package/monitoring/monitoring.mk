150520287a25b92ad8b00809d861436aef399fc7
Branko Vukelic <branko@outernet.is>
2015-12-11 17:37:54 +0100

    Updated monitoring
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- package/monitoring/monitoring.mk
+++ package/monitoring/monitoring.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MONITORING_VERSION = 2707b9c1048dab29f119b8e6577e00a3ae709c46
+MONITORING_VERSION = c94f86e088cad1df05ca809d07eed1004bcaa7df
 MONITORING_SOURCE = $(MONITORING_VERSION).tar.gz
 MONITORING_SITE = https://github.com/Outernet-Project/monitoring/archive
 MONITORING_LICENSE = GPL
