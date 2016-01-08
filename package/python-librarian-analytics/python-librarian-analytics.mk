e00118061763081d802f3405c954f8fd6c8fffc2
Branko Vukelic <branko@outernet.is>
2016-01-08 16:18:36 +0100

    Updated librarian-analytics
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- package/python-librarian-analytics/python-librarian-analytics.mk
+++ package/python-librarian-analytics/python-librarian-analytics.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_ANALYTICS_VERSION = c3a55441c19001dada36be403e7e2e47e5728533
+PYTHON_LIBRARIAN_ANALYTICS_VERSION = 7bfeaaa4e4d4b4c90915a6f826f91aeb5393da84
 PYTHON_LIBRARIAN_ANALYTICS_SOURCE = $(PYTHON_LIBRARIAN_ANALYTICS_VERSION).tar.gz
 PYTHON_LIBRARIAN_ANALYTICS_SITE = https://github.com/Outernet-Project/librarian-analytics/archive
 PYTHON_LIBRARIAN_ANALYTICS_LICENSE = GPL
